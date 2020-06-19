9f9f98a406b6aeaff2e0cf2f73d7d905a9045eeb
kazukinagata <KNagata8@gmail.com>
2020-06-19 13:03:21 +0900

    mainをxmlParser.jsにする

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     output: {
       path: path.join(__dirname, 'dist'),
       filename: 'bundle.js',
-      libraryTarget: "commonjs2",
+      libraryTarget: "commonjs",
       globalObject: 'this',
     },
     resolve: {
