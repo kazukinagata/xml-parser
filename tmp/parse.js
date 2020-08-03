24a30f0c7bfa026d4aa320f03a53c0baaa418ed6
kazukinagata <KNagata8@gmail.com>
2020-08-03 13:58:13 +0900

    git rm --cached

--- tmp/parse.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-const XMLParser = require('../xmlParser')
-const xmlParser = new XMLParser()
-
-const buf = fs.readFileSync('./demo.svg')
-const tree = xmlParser.parseFromString(buf.toString())
-console.log(tree)
