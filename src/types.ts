9b4f374a6ec95db8663da0e77ae8cb05cdab82cc
kazukinagata <KNagata8@gmail.com>
2020-08-03 20:14:55 +0900

    Change attributes.style type to any

--- src/types.ts
+++ src/types.ts
@@ -2,7 +2,7 @@ import React from 'react'
 export interface Tree {
   name: string
   attributes: {
-    style?: React.CSSProperties | {[x: string]: string}
+    style?: any
     [x: string]: any
   }
   children: Tree[]
